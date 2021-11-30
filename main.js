2be51c9d7bb291a9d01063b7f6a478d9497688b8
Tahir Ovayurt <iovayurt@gmail.com>
2021-11-30 11:47:34 +0300

    animation

--- main.js
+++ main.js
@@ -75,11 +75,28 @@ const moon = new THREE.Mesh(
 
 scene.add(moon);
 
-moon.position.z = 30;
+moon.position.z = 20;
 moon.position.setX(-10);
 
 ilyas.position.z = -5;
 ilyas.position.x = 2;
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  ilyas.rotation.y += 0.01;
+  ilyas.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
 
 
 function animate() {
